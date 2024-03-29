8c21abf2ecd94bf875b1011a58891a83c2947eb9
Gowthamahendiran <gowthamahendiran@gmail.com>
2024-03-29 11:49:27 +0530

    updateProfile

--- App.js
+++ App.js
@@ -62,6 +62,26 @@ app.get('/employees', async (req, res) => {
     }
 });
 
+
+app.put('/update-profile-picture/:userId', upload.single('profileImage'), async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const profileImage = req.file.path;
+
+        const user = await User.findByIdAndUpdate(userId, { profileImage }, { new: true });
+
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+
+        res.status(200).json({ message: 'Profile picture updated successfully', user });
+    } catch (error) {
+        res.status(500).json({ message: 'Failed to update profile picture', error });
+    }
+});
+
+
+
 app.post('/timesheet/timein/:userId', async (req, res) => {
     try {
         const userId = req.params.userId;
